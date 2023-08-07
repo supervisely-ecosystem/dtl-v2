0d43b37db130c9f48a2737b307e9698f214fabfd
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-08-07 12:33:05 -0300

    add create_info_widget() and parse_options()

--- src/ui/dtl/actions/base.py
+++ src/ui/dtl/actions/base.py
@@ -1,21 +1,44 @@
-from supervisely.app.widgets import NodesFlow
+from supervisely.app.widgets import NodesFlow, Container, Text
 
 
 class Action:
     name = None
     title = None
+    docs_url = None
+    description = None
+    width = 200
 
     @classmethod
     def create_options(cls):
         raise NotImplementedError
-    
+
     @classmethod
     def create_inputs(cls):
         return [NodesFlow.Node.Input("source", "Source")]
-    
+
     @classmethod
     def create_outputs(cls):
         return [NodesFlow.Node.Output("destination", "Destination")]
+
+    @classmethod
+    def create_info_widget(cls):
+        return Container(
+            widgets=[
+                Text(f"<h3>{cls.title}</h3>", color="white"),
+                Text(
+                    f'<a href="{cls.docs_url}" target="_blank" style="color: white;">Docs</a>'
+                ),
+                Text(f"<p>{cls.description}</p>", color="white"),
+            ]
+        )
+
+    @classmethod
+    def parse_options(cls, options: dict) -> dict:
+        return {
+            "src": [],
+            "dst": [],
+            "settings": {},
+        }
 
 
 class Layer:
