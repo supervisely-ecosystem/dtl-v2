fd7da49d8d6a4f3aacabb61fdca6e7f2cc08b449
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-08-07 12:35:01 -0300

    add parse_options() and options update

--- src/ui/dtl/actions/data.py
+++ src/ui/dtl/actions/data.py
@@ -1,38 +1,64 @@
+import copy
+import json
 from .base import Action
-from supervisely.app.widgets import NodesFlow, Text
+from supervisely.app.widgets import NodesFlow
+
 
 class DataAction(Action):
-    title = "Data"
     name = "data"
+    title = "Data"
     docs_url = "https://docs.supervisely.com/data-manipulation/index/data-layers/data"
+    description = "Data layer (data) is used to specify project and its datasets that will participate in data transformation process."
 
     @classmethod
     def create_options(cls):
         return [
             NodesFlow.Node.Option(
-                name = "Info",
-                option_component = NodesFlow.ButtonOptionComponent(
-                    sidebar_component=NodesFlow.WidgetOptionComponent(Text(cls.docs_url))
-                )
+                name="Info",
+                option_component=NodesFlow.ButtonOptionComponent(
+                    sidebar_component=NodesFlow.WidgetOptionComponent(
+                        cls.create_info_widget()
+                    )
+                ),
             ),
             NodesFlow.Node.Option(
-                name = "source_text",
-                option_component = NodesFlow.TextOptionComponent("Source")
+                name="source_text",
+                option_component=NodesFlow.TextOptionComponent("Source"),
             ),
             NodesFlow.Node.Option(
-                name = "src",
-                option_component = NodesFlow.InputOptionComponent()
+                name="src", option_component=NodesFlow.InputOptionComponent()
             ),
             NodesFlow.Node.Option(
-                name = "classes_mapping_text",
-                option_component = NodesFlow.TextOptionComponent("Classes Mapping")
+                name="classes_mapping_text",
+                option_component=NodesFlow.TextOptionComponent("Classes Mapping"),
             ),
             NodesFlow.Node.Option(
-                name = "classes_mapping",
-                option_component = NodesFlow.InputOptionComponent()
+                name="classes_mapping",
+                option_component=NodesFlow.InputOptionComponent(),
             ),
         ]
-    
+
     @classmethod
     def create_inputs(cls):
         return []
+    
+    @classmethod
+    def parse_options(cls, options: dict) -> dict:
+        src = options["src"]
+        if src is None or src == "":
+            raise ValueError("Source is not specified")
+        if src[0] == "[":
+            src = json.loads(src)
+        else:
+            src = [src.strip("'\"")]
+        classes_mapping = options["classes_mapping"]
+        if classes_mapping[0] == "{":
+            classes_mapping = json.loads(classes_mapping)
+        else:
+            classes_mapping = classes_mapping.strip("'\"")
+        return {
+            "src": src,
+            "settings": {
+                "classes_mapping": classes_mapping
+            }
+        }
--- src/ui/dtl/actions/dataset.py
+++ src/ui/dtl/actions/dataset.py
@@ -1,36 +1,50 @@
 from .base import Action
-from supervisely.app.widgets import NodesFlow, Text
+from supervisely.app.widgets import NodesFlow
 
 
 class DatasetAction(Action):
-    title = "Dataset"
     name = "dataset"
-    description = "This layer (dataset) places every image that it sees to dataset with a specified name."
+    title = "Dataset"
     docs_url = "https://docs.supervisely.com/data-manipulation/index/transformation-layers/approx_vector"
+    description = "This layer (dataset) places every image that it sees to dataset with a specified name. Put name of the future dataset in the field name."
 
     @classmethod
     def create_options(cls):
         return [
             NodesFlow.Node.Option(
-                name = "Info",
-                option_component = NodesFlow.ButtonOptionComponent(
-                    sidebar_component=NodesFlow.WidgetOptionComponent(Text(f'{cls.description}\n{cls.docs_url}'))
-                )
+                name="Info",
+                option_component=NodesFlow.ButtonOptionComponent(
+                    sidebar_component=NodesFlow.WidgetOptionComponent(
+                        cls.create_info_widget()
+                    )
+                ),
             ),
             NodesFlow.Node.Option(
-                name = "rule_text",
-                option_component = NodesFlow.TextOptionComponent("Rule: Save Original")
+                name="rule_text",
+                option_component=NodesFlow.TextOptionComponent("Rule: Save Original"),
             ),
             NodesFlow.Node.Option(
-                name = "rule",
-                option_component = NodesFlow.CheckboxOptionComponent()
+                name="Save original", option_component=NodesFlow.CheckboxOptionComponent()
             ),
             NodesFlow.Node.Option(
-                name = "name_text",
-                option_component = NodesFlow.TextOptionComponent("Name")
+                name="name_text", option_component=NodesFlow.TextOptionComponent("Name")
             ),
             NodesFlow.Node.Option(
-                name = "name",
-                option_component = NodesFlow.InputOptionComponent()
-            )
+                name="name", option_component=NodesFlow.InputOptionComponent()
+            ),
         ]
+
+    @classmethod
+    def parse_options(cls, options: dict) -> dict:
+        if options["Save original"]:
+            return {
+                "settings": {
+                    "rule": "save_original",
+                }
+            }
+        name = options["name"].strip("'\"").lstrip("'\"")
+        return {
+            "settings": {
+                "name": name,
+            },
+        }
