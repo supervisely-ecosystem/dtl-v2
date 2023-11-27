062c5a0b3a9ffc29ab71ae3b76911a7c4a332d74
cxnt <pavelbartsits@gmail.com>
2023-11-27 18:53:14 +0400

    move convert method to utils

--- src/compute/dtl_utils/__init__.py
+++ src/compute/dtl_utils/__init__.py
@@ -1,5 +1,13 @@
 from typing import Union, Callable
-from supervisely import Annotation, VideoAnnotation, FrameCollection
+from supervisely import (
+    Annotation,
+    VideoAnnotation,
+    FrameCollection,
+    ProjectMeta,
+    Frame,
+    VideoObject,
+    VideoObjectCollection,
+)
 
 
 def apply_to_labels(ann: Union[Annotation, VideoAnnotation], fn: Callable):
@@ -19,3 +27,39 @@ def apply_to_labels(ann: Union[Annotation, VideoAnnotation], fn: Callable):
         frames_col = FrameCollection(new_frames)
         ann = ann.clone(frames=frames_col)
     return ann
+
+
+def convert_video_annotation(ann: VideoAnnotation, dst_meta: ProjectMeta):
+    frames = []
+    new_objects = {}
+
+    for curr_object in ann.objects:
+        new_obj_class = dst_meta.obj_classes.get(curr_object.obj_class.name)
+        if curr_object.obj_class.geometry_type == new_obj_class.geometry_type:
+            new_objects[curr_object.key] = curr_object
+        else:
+            new_object = VideoObject(obj_class=new_obj_class, tags=curr_object.tags)
+            new_objects[curr_object.key] = new_object
+
+    for curr_frame in ann.frames:
+        new_frame_figures = []
+        for curr_figure in curr_frame.figures:
+            curr_figure_obj_class = curr_figure.video_object.obj_class
+            new_obj_class = dst_meta.obj_classes.get(curr_figure_obj_class.name)
+            if curr_figure_obj_class.geometry_type == new_obj_class.geometry_type:
+                new_frame_figures.append(curr_figure)
+            else:
+                new_geometries = curr_figure.geometry.convert(new_obj_class.geometry_type)
+                for new_geometry in new_geometries:
+                    new_figure = curr_figure.clone(
+                        video_object=new_objects[curr_figure.video_object.key],
+                        geometry=new_geometry,
+                    )
+                    new_frame_figures.append(new_figure)
+        new_frame = Frame(curr_frame.index, new_frame_figures)
+        frames.append(new_frame)
+
+    new_frames_collection = FrameCollection(frames)
+    new_objects = VideoObjectCollection(list(new_objects.values()))
+
+    return ann.clone(objects=new_objects, frames=new_frames_collection)
--- src/compute/layers/processing/BBoxLayer.py
+++ src/compute/layers/processing/BBoxLayer.py
@@ -1,56 +1,10 @@
 # coding: utf-8
 
-from supervisely import (
-    Rectangle,
-    Label,
-    Annotation,
-    VideoAnnotation,
-    Frame,
-    VideoObject,
-    FrameCollection,
-    VideoObjectCollection,
-    ProjectMeta,
-)
+from supervisely import Rectangle, Label, Annotation
 
 from src.compute.Layer import Layer
 from src.compute.classes_utils import ClassConstants
-from src.compute.dtl_utils import apply_to_labels
-
-
-def convert_objects_to_bbox(ann: VideoAnnotation, dst_meta: ProjectMeta):
-    frames = []
-    new_objects = {}
-
-    for curr_object in ann.objects:
-        new_obj_class = dst_meta.obj_classes.get(curr_object.obj_class.name)
-        if curr_object.obj_class.geometry_type == new_obj_class.geometry_type:
-            new_objects[curr_object.key] = curr_object
-        else:
-            new_object = VideoObject(obj_class=new_obj_class, tags=curr_object.tags)
-            new_objects[curr_object.key] = new_object
-
-    for curr_frame in ann.frames:
-        new_frame_figures = []
-        for curr_figure in curr_frame.figures:
-            curr_figure_obj_class = curr_figure.video_object.obj_class
-            new_obj_class = dst_meta.obj_classes.get(curr_figure_obj_class.name)
-            if curr_figure_obj_class.geometry_type == new_obj_class.geometry_type:
-                new_frame_figures.append(curr_figure)
-            else:
-                new_geometries = curr_figure.geometry.convert(new_obj_class.geometry_type)
-                for new_geometry in new_geometries:
-                    new_figure = curr_figure.clone(
-                        video_object=new_objects[curr_figure.video_object.key],
-                        geometry=new_geometry,
-                    )
-                    new_frame_figures.append(new_figure)
-        new_frame = Frame(curr_frame.index, new_frame_figures)
-        frames.append(new_frame)
-
-    new_frames_collection = FrameCollection(frames)
-    new_objects = VideoObjectCollection(list(new_objects.values()))
-
-    return ann.clone(objects=new_objects, frames=new_frames_collection)
+from src.compute.dtl_utils import apply_to_labels, convert_video_annotation
 
 
 class BBoxLayer(Layer):
@@ -81,7 +35,7 @@ class BBoxLayer(Layer):
         self.cls_mapping[ClassConstants.OTHER] = ClassConstants.DEFAULT
 
     def process(self, data_el):
-        img_desc, ann = data_el
+        item_desc, ann = data_el
 
         def to_fig_rect(label: Label):
             new_title = self.settings["classes_mapping"].get(label.obj_class.name)
@@ -99,5 +53,5 @@ class BBoxLayer(Layer):
         if isinstance(ann, Annotation):
             ann = apply_to_labels(ann, to_fig_rect)
         else:
-            ann = convert_objects_to_bbox(ann, self.output_meta)
-        yield img_desc, ann
+            ann = convert_video_annotation(ann, self.output_meta)
+        yield item_desc, ann
