8873e566ad58565b1e7fcfa48286e611a577e13b
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-12-01 16:04:40 -0300

    FilterImageByObject bugfix

--- src/compute/layers/processing/FilterImageByObject.py
+++ src/compute/layers/processing/FilterImageByObject.py
@@ -31,17 +31,14 @@ class FilterImageByObjectLayer(Layer):
     def process(self, data_el: Tuple[ImageDescriptor, Annotation]):
         img_desc, ann = data_el
 
-        satisfies_cond = True
         include_classes = self.settings["include"]
         exclude_classes = self.settings["exclude"]
 
-        for label in ann.labels:
-            if label.obj_class.name in exclude_classes:
-                satisfies_cond = False
-                break
-            if label.obj_class.name not in include_classes:
-                satisfies_cond = False
-                break
+        img_classes = {label.obj_class.name for label in ann.labels}
+
+        satisfies_cond = all(cls in img_classes for cls in include_classes) and all(
+            cls not in img_classes for cls in exclude_classes
+        )
 
         if satisfies_cond:
             yield data_el + tuple([0])  # branch 0
