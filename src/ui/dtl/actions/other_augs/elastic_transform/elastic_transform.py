6038b59775c3032c741e84909e089adec7119c02
vorozhkog <vorozkhog@icloud.com>
2024-05-02 12:33:37 +0100

    branch release

--- src/ui/dtl/actions/other_augs/elastic_transform/elastic_transform.py
+++ src/ui/dtl/actions/other_augs/elastic_transform/elastic_transform.py
@@ -3,9 +3,10 @@ from os.path import realpath, dirname
 
 from src.ui.dtl import OtherAugmentationsAction
 from src.ui.dtl.Layer import Layer
-from src.ui.dtl.utils import get_layer_docs
+from src.ui.dtl.utils import get_layer_docs, get_text_font_size
 
 from supervisely.app.widgets import (
+    Text,
     NodesFlow,
     InputNumber,
     Field,
@@ -21,26 +22,37 @@ class ElasticTransformAction(OtherAugmentationsAction):
 
     @classmethod
     def create_new_layer(cls, layer_id: Optional[str] = None):
-        severity_input = InputNumber(value=2, min=1, max=5, step=1, size="small")
-        severity_field = Field(
-            title="Severity",
-            description="",
-            content=severity_input,
-        )
+        alpha_text = Text("Alpha", status="text", font_size=get_text_font_size())
+        alpha_input = InputNumber(value=10.000, step=0.1, precision=3, controls=True, size="small")
+
+        sigma_text = Text("Sigma", status="text", font_size=get_text_font_size())
+        sigma_input = InputNumber(value=1.000, step=0.1, controls=True, size="small")
 
         def get_settings(options_json: dict) -> dict:
-            return {"severity": severity_input.get_value()}
+            return {"alpha": alpha_input.get_value(), "sigma": sigma_input.get_value()}
 
         def _set_settings_from_json(settings: dict):
-            severity = settings.get("severity", 2)
-            severity_input.value = severity
+            alpha_input.value = settings.get("alpha", 10)
+            sigma_input.value = settings.get("sigma", 1)
 
         def create_options(src: list, dst: list, settings: dict) -> dict:
             _set_settings_from_json(settings)
             settings_options = [
                 NodesFlow.Node.Option(
-                    name="Severity",
-                    option_component=NodesFlow.WidgetOptionComponent(severity_field),
+                    name="alpha_text",
+                    option_component=NodesFlow.WidgetOptionComponent(alpha_text),
+                ),
+                NodesFlow.Node.Option(
+                    name="alpha",
+                    option_component=NodesFlow.WidgetOptionComponent(alpha_input),
+                ),
+                NodesFlow.Node.Option(
+                    name="sigma_text",
+                    option_component=NodesFlow.WidgetOptionComponent(sigma_text),
+                ),
+                NodesFlow.Node.Option(
+                    name="sigma",
+                    option_component=NodesFlow.WidgetOptionComponent(sigma_input),
                 ),
             ]
             return {
