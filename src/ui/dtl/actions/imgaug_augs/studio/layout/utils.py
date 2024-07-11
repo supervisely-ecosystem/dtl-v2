cb880cae240b07eb6befbcc804bd73365c2e5d1f
cxnt <pavelbartsits@gmail.com>
2024-07-11 11:59:20 +0400

    Remove unused code

--- src/ui/dtl/actions/imgaug_augs/studio/layout/utils.py
+++ src/ui/dtl/actions/imgaug_augs/studio/layout/utils.py
@@ -1,6 +1,5 @@
 import os
 import json
-from collections import OrderedDict
 from typing import List, Dict
 from supervisely.app.widgets import (
     Select,
@@ -11,85 +10,10 @@ from supervisely.app.widgets import (
     Widget,
     Field,
 )
-import supervisely as sly
-
-import src.globals as g
-
-
-json_path = os.path.join(
-    os.getcwd(), "src", "ui", "dtl", "actions", "imgaug_augs", "studio", "layout", "augs.json"
-)
-
-augs_configs = sly.json.load_json_file(json_path)
-meta = g.api.project.get_meta(g.PROJECT_ID)
-
-pipeline = []
-
-images_info = {}
-all_images = []
-
-
-def _normalize_params(default_params: dict, params: dict):
-    ptypes = {p["pname"]: p for p in default_params}
-    res = OrderedDict()
-    for name, value in params.items():
-        if ptypes[name]["type"] == "el-slider-range":
-            res[name] = tuple(value)
-        elif (
-            ptypes[name]["type"] == "el-input-number" and ptypes[name].get("valueType") is not None
-        ):
-            try:
-                res[name] = int(value)
-            except ValueError as e:
-                res[name] = int(ptypes[name]["default"])
-        elif ptypes[name]["type"] == "el-input-number":
-            try:
-                res[name] = float(value)
-            except ValueError as e:
-                res[name] = float(ptypes[name]["default"])
-        elif ptypes[name]["type"] == "el-input-range":
-            try:
-                res[name] = tuple([int(value[0]), int(value[1])])
-            except ValueError as e:
-                res[name] = tuple(ptypes[name]["default"])
-        elif ptypes[name]["type"] == "el-input-number-range":
-            try:
-                res[name] = [int(value[0]), int(value[1])]
-                res[name].sort()
-                res[name] = tuple(res[name])
-            except ValueError as e:
-                res[name] = tuple(ptypes[name]["default"])
-        else:
-            res[name] = value
-    return res
-
-
-def get_aug_info(state):
-    category_name = state["category"]
-    aug_name = state["aug"]
-    ui_defaults = augs_configs[category_name][aug_name]["params"]
-    ui_vmodels = state["augVModels"][category_name][aug_name]
-    sometimes = float(state["sometimesP"]) if state["sometimes"] else None
-    params = _normalize_params(ui_defaults, ui_vmodels)
-    aug_info = sly.imgaug_utils.create_aug_info(category_name, aug_name, params, sometimes)
-    return aug_info
-
-
-def save_preview_image(api: sly.Api, task_id, img):
-    local_path = g.PREVIEW_DIR
-    remote_path = os.path.join(f"/imgaug_studio/{task_id}", f"last_preview.png")
-    sly.image.write(local_path, img)
-    if api.file.exists(g.TEAM_ID, remote_path):
-        api.file.remove(g.TEAM_ID, remote_path)
-    file_info = api.file.upload(g.TEAM_ID, local_path, remote_path)
-    return file_info
 
 
 json_path = os.path.join(os.getcwd(), "src/ui/dtl/actions/imgaug_augs/studio/layout/augs.json")
-
-augs_json = ""
-with open(json_path, "r") as file:
-    augs_json = json.load(file)
+augs_json = json.load(json_path)
 
 
 def get_params_widget(category, func):
