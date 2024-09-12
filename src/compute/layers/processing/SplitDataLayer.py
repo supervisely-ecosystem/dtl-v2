61a8dd06e83fa836b4a7c93636369954d639b38c
vorozhkog <vorozkhog@icloud.com>
2024-09-12 11:28:18 +0100

    wip

--- src/compute/layers/processing/SplitDataLayer.py
+++ src/compute/layers/processing/SplitDataLayer.py
@@ -33,12 +33,37 @@ class SplitDataLayer(Layer):
         return True
 
     def process(self, data_el: Tuple[ImageDescriptor, Annotation]):
-        def _split() -> List[str]:
-            pass
 
-        method = self.settings.get("method", 0)
+        def _split_by_percent(orig_ann):
+            raise NotImplementedError
+
+        def _split_by_num(orig_ann):
+            raise NotImplementedError
+
+        def _split_by_class(orig_ann):
+            image_labels = orig_ann.labels
+            if len(image_labels) > 0:
+                classes = [label.obj_class for label in image_labels]
+            raise NotImplementedError
+
+        def _split_by_tags(orig_ann):
+            image_tags = orig_ann.img_tags
+            if len(image_tags) == 0:
+                pass
+            raise NotImplementedError
+
+        idx_to_func = {
+            0: _split_by_percent,
+            1: _split_by_num,
+            3: _split_by_class,
+            4: _split_by_tags,
+        }
         img_desc, orig_ann = data_el
-        # meta = self.output_meta
+        method_idx = self.settings.get("method", 0)
+        meta = self.output_meta
+
+        func = idx_to_func.get(method_idx)
+        func(orig_ann)
 
         new_img_desc = deepcopy(img_desc)
         yield (new_img_desc, orig_ann)
