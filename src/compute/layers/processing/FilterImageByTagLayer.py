25a64f3c2f8b6d6314809eb69d3846edf75885dc
cxnt <pavelbartsits@gmail.com>
2024-10-07 16:12:16 +0400

    Add option to filter by object tags to FilterImageByTag layer

--- src/compute/layers/processing/FilterImageByTagLayer.py
+++ src/compute/layers/processing/FilterImageByTagLayer.py
@@ -30,7 +30,10 @@ class FilterImageByTagLayer(Layer):
                             },
                         },
                     },
-                    "condition": {"type": "string", "enum": ["with", "without"]},
+                    "condition": {
+                        "type": "string",
+                        "enum": ["with_img", "without_img", "with_obj", "without_obj"],
+                    },
                 },
             }
         },
@@ -46,20 +49,44 @@ class FilterImageByTagLayer(Layer):
         img_desc, ann = data_el
         condition = self.settings["condition"]
 
-        def has_tag(img_tags: Union[List[Tag], TagCollection], filter_tag: dict):
+        def has_tag_img(img_tags: Union[List[Tag], TagCollection], filter_tag: dict):
             for img_tag in img_tags:
                 if img_tag.name == filter_tag["name"] and img_tag.value == filter_tag["value"]:
                     return True
             return False
 
-        if condition == "with":
-            satisfies_cond = all(has_tag(ann.img_tags, f_tag) for f_tag in self.settings["tags"])
-        elif condition == "without":
-            satisfies_cond = all(
-                not has_tag(ann.img_tags, f_tag) for f_tag in self.settings["tags"]
-            )
+        def has_tag_obj(obj_tags: Union[List[Tag], TagCollection], filter_tag: dict):
+            for obj_tag in obj_tags:
+                if obj_tag.name == filter_tag["name"] and obj_tag.value == filter_tag["value"]:
+                    return True
+            return False
+
+        if condition in ["with_img", "without_img"]:
+            if condition == "with_img":
+                satisfies_cond = all(
+                    has_tag_img(ann.img_tags, f_tag) for f_tag in self.settings["tags"]
+                )
+            elif condition == "without_img":
+                satisfies_cond = all(
+                    not has_tag_img(ann.img_tags, f_tag) for f_tag in self.settings["tags"]
+                )
+
+        elif condition in ["with_obj", "without_obj"]:
+            obj_tags = []
+            for label in ann.labels:
+                obj_tags.extend(label.tags)
+
+            if condition == "with_obj":
+                satisfies_cond = all(
+                    has_tag_obj(obj_tags, f_tag) for f_tag in self.settings["tags"]
+                )
+            elif condition == "without_obj":
+                satisfies_cond = all(
+                    not has_tag_obj(obj_tags, f_tag) for f_tag in self.settings["tags"]
+                )
         else:
             raise NotImplementedError()
+
         if satisfies_cond:
             yield data_el + tuple([0])  # branch 0
         else:
